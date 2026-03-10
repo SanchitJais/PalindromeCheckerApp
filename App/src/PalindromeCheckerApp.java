50b40fc69aad5d1e9439e0e29cb4f71b8918195e
Sanchit Jaiswal <sanchit.jaiswal2006@gmail.com>
2026-03-10 10:07:27 +0530

    UC5: Palindrome Logic Implementation

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+public class Palindrome {
+    static void main() {
+        String input = "madam";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length(); i++) {
+            char poppedChar = stack.pop();
+            if (input.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PalindromeCheckerApp
-Palindrome Checker is a simple app that determines whether a word, phrase, or sentence reads the same forward and backward. It ignores spaces, punctuation, and letter casing for accurate results. The input is cleaned and compared to its reversed version. Lightweight, fast, and easy to use. 
+PalindromeCheckerApp Checker is a simple app that determines whether a word, phrase, or sentence reads the same forward and backward. It ignores spaces, punctuation, and letter casing for accurate results. The input is cleaned and compared to its reversed version. Lightweight, fast, and easy to use. 
